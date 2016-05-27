86dca9d475b8e8d20adf732baada99b784d2c076
bonino <dario.bonino@gmail.com>
2016-05-27 07:34:27 +0200

    Done some polishing, minor additions to the test class

--- org.doggateway.libraries.intel.tinyb/src/org/doggateway/libraries/intel/tinyb/example/HelloTinyB.java
+++ org.doggateway.libraries.intel.tinyb/src/org/doggateway/libraries/intel/tinyb/example/HelloTinyB.java
@@ -1,3 +1,20 @@
+/*
+ * Dog - Bluetooth Low Energy OSGi wrapper for Intel TinyB
+ * 
+ * Copyright (c) 2016 Dario Bonino 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 package org.doggateway.libraries.intel.tinyb.example;
 
 import tinyb.*;
@@ -61,9 +78,15 @@ public class HelloTinyB {
         List<BluetoothGattService> bluetoothServices = null;
         do {
             bluetoothServices = device.getServices();
+            
 
             for (BluetoothGattService service : bluetoothServices) {
                 System.out.println("UUID: " + service.getUuid());
+                
+                List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+                for(BluetoothGattCharacteristic characteristic : characteristics)
+                	System.out.println("Char UUID: " + characteristic.getUuid());
+                
                 if (service.getUuid().equals(UUID))
                     tempService = service;
             }
--- org.doggateway.libraries.intel.tinyb/src/org/doggateway/libraries/intel/tinyb/service/BluetoothServiceImpl.java
+++ org.doggateway.libraries.intel.tinyb/src/org/doggateway/libraries/intel/tinyb/service/BluetoothServiceImpl.java
@@ -1,5 +1,19 @@
-/**
+/*
+ * Dog - Bluetooth Low Energy OSGi wrapper for Intel TinyB
  * 
+ * Copyright (c) 2016 Dario Bonino 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
 package org.doggateway.libraries.intel.tinyb.service;
 
@@ -13,7 +27,7 @@ import tinyb.BluetoothGattService;
 import tinyb.BluetoothManager;
 
 /**
- * @author bonino
+ * @author <a href="mailto:dario.bonino@gmail.com">Dario Bonino</a>
  *
  */
 public class BluetoothServiceImpl implements BluetoothService
